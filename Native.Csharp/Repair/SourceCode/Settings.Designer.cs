087a878e1acface549d43a01e5c9d5bc9f5524ff
myderr <1457522662@qq.com>
2019-05-20 20:05:00 +0800

    添加项目文件。

---
+++ Native.Csharp/Repair/SourceCode/Settings.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Native.Csharp.Repair.SourceCode
+{
+    partial class Settings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaster = new System.Windows.Forms.TextBox();
+            this.btnSure = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(85, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "设置主人：";
+            // 
+            // txtMaster
+            // 
+            this.txtMaster.Location = new System.Drawing.Point(156, 31);
+            this.txtMaster.Name = "txtMaster";
+            this.txtMaster.Size = new System.Drawing.Size(100, 21);
+            this.txtMaster.TabIndex = 1;
+            // 
+            // btnSure
+            // 
+            this.btnSure.Location = new System.Drawing.Point(132, 68);
+            this.btnSure.Name = "btnSure";
+            this.btnSure.Size = new System.Drawing.Size(75, 23);
+            this.btnSure.TabIndex = 2;
+            this.btnSure.Text = "确定(&O)";
+            this.btnSure.UseVisualStyleBackColor = true;
+            this.btnSure.Click += new System.EventHandler(this.BtnSure_Click);
+            // 
+            // Settings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 103);
+            this.Controls.Add(this.btnSure);
+            this.Controls.Add(this.txtMaster);
+            this.Controls.Add(this.label1);
+            this.Name = "Settings";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "设置页面";
+            this.Load += new System.EventHandler(this.Setting_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaster;
+        private System.Windows.Forms.Button btnSure;
+    }
+}
---
+++ Native.Csharp/Repair/SourceCode/Settings.cs
@@ -0,0 +1,55 @@
+﻿using Native.Csharp.App;
+using Native.Csharp.Tool.IniConfig.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Native.Csharp.Repair.SourceCode
+{
+    public partial class Settings : Form
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+
+        private void Setting_Load(object sender, EventArgs e)
+        {
+            string strPath = Common.AppDirectory + "//设置.ini";
+            if (File.Exists(strPath))
+            {
+                IniObject iObject = IniObject.Load(strPath, Encoding.Default);
+                if (iObject.ContainsKey("设置主人"))
+                {
+                    IniSection section1 = iObject["设置主人"];
+                    if (section1.ContainsKey("主人QQ"))
+                    {
+                        txtMaster.Text = section1["主人QQ"].Value;
+                    }
+                }
+            }
+        }
+
+        private void BtnSure_Click(object sender, EventArgs e)
+        {
+            string strPath = Common.AppDirectory + "//设置.ini";
+            string strMaster = txtMaster.Text;
+            IniObject iObject = new IniObject();
+            if (File.Exists(strPath))
+            {
+                iObject = IniObject.Load(strPath, Encoding.Default);
+            }
+            IniSection iSection = new IniSection("设置主人");
+            iSection.Add("主人QQ", strMaster);
+            iObject.Add(iSection);
+            iObject.Save(strPath);
+            this.Close();
+        }
+    }
+}
